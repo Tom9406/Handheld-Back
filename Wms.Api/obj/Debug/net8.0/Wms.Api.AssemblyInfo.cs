abc37b587fc56ca68d467431a9dc62bb50d73736
Tony <tonialv94@gmail.com>
2026-03-04 14:39:39 -0300

    revert

--- Wms.Api/obj/Debug/net8.0/Wms.Api.AssemblyInfo.cs
+++ Wms.Api/obj/Debug/net8.0/Wms.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wms.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49ced559afc9eb9b943ddbd95046ffcb27f23f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eefb74257b06b5f025c1fcbcb317c643cea346e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wms.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wms.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
